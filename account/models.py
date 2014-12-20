c483f07d0d6bde2942f8afea7a77f399a79d85bd
Ondrej Sika <ondrej@ondrejsika.com>
2014-12-20 01:12:06 +0100

    sync emails (save + signal)

--- account/models.py
+++ account/models.py
@@ -1,5 +1,6 @@
 # django
 from django.db import models
+from django.db.models.signals import post_save
 
 # django contrib
 from django.contrib.auth.models import User
@@ -15,3 +16,19 @@ class Account(models.Model):
 
     def __unicode__(self):
         return u'%s' % self.user
+
+    def save(self, update_user=True, **kwargs):
+        if update_user:
+            self.user.email = self.email
+            self.user.save()
+        super(Account, self).save(**kwargs)
+
+
+def user_post_save(sender, instance, created, **kwargs):
+    if not instance.account:
+        instance.account = Account(user=instance, email=instance.email)
+    instance.account.email = instance.email
+    instance.account.save(update_user=False)
+
+
+post_save.connect(user_post_save, sender=User)
