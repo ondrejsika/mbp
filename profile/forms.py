96117733b5737455f71f010f24d60bd3699664ed
Ondrej Sika <ondrej@ondrejsika.com>
2014-12-20 20:46:37 +0100

    add default xpub

--- profile/forms.py
+++ profile/forms.py
@@ -1,5 +1,6 @@
 # django
 from django import forms
+from django.conf import settings
 
 # local
 from .models import Profile
@@ -12,14 +13,17 @@ class ProfileForm(forms.ModelForm):
 
     def __init__(self, account, *args, **kwargs):
         self.account = account
+        self.instance = kwargs['instance']
         super(ProfileForm, self).__init__(*args, **kwargs)
 
-        if not account.priv_editable_xpubs and kwargs['instance']:
+        if not account.priv_editable_xpubs:
             del self.fields['xpub']
 
     def save(self, commit=True):
         obj = super(ProfileForm, self).save(commit=False)
         obj.account = self.account
+        if not obj.xpub:
+            obj.xpub = settings.DEFAULT_XPUB
         if commit == True:
             obj.save()
         return obj
