a1c4be28f00aca93cf74311b05acf7b12c61abde
Ondrej Sika <ondrej@ondrejsika.com>
2014-12-20 22:57:24 +0100

    add static root

--- settings.py
+++ settings.py
@@ -107,6 +107,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 ORIGIN = 'http://localhost:8000'
 
