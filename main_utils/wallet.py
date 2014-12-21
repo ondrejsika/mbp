cf01c7f9054806c3698dbe7aa684da761ce73966
Ondrej Sika <ondrej@ondrejsika.com>
2014-12-21 23:00:27 +0100

    fix wallet derivation

--- main_utils/wallet.py
+++ main_utils/wallet.py
@@ -3,4 +3,4 @@ from bip32utils import BIP32Key
 
 
 def get_wallet_from_xpub(xpub, no):
-    return BIP32Key.fromExtendedKey(xpub).ChildKey(no).Address()
+    return BIP32Key.fromExtendedKey(xpub).ChildKey(0).ChildKey(no).Address()
