46671990cdc612d2327d4ef8d637650d03af5cd1
Ondrej Sika <sika.ondrej@gmail.com>
2016-02-14 21:21:02 +0100

    fix confirm transactions

--- main_utils/blockchain_utils.py
+++ main_utils/blockchain_utils.py
@@ -1,6 +1,14 @@
+# python
+from decimal import Decimal
+
 # lib
 from blockchain.blockexplorer import get_address
 
 
+def sat_to_btc(sat):
+    return Decimal(str(sat / float(10**8)))
+
+
 def get_wallet_amount(wallet):
-    return get_address(wallet).total_received
+    return sat_to_btc(get_address(wallet).total_received)
+
